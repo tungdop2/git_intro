6113c276262eb8e6fecb6a8fa793a368ff750d13
Qtieee <tungdop2@gmail.com>
2022-04-05 11:02:20 +0700

    feat(calculation) add rectangle_area

--- calculation.py
+++ calculation.py
@@ -6,10 +6,14 @@ def cal_rectangle_perimeter(a, b):
 def cal_circle_area(r):
     return math.pi * pow(r, 2)
 
+def cal_rectangle_area(a, b):
+    return a * b
+
 if __name__ == '__main__':
     f = int(input("choose function: \n\
         0. cal_rectangle_perimeter \n\
-        1. cal_circle_area \nPlease enter an integer:"))
+        1. cal_circle_area \n\
+        2. cal_rectangle_area \nPlease enter an integer:"))
 
     mult = int(input("Add multiplier:"))
 
@@ -21,7 +25,12 @@ if __name__ == '__main__':
     elif f == 1:
         r = int(input("Input value r: "))
         result = cal_circle_area(r)
-        
+    
+    elif f == 2:
+        a = int(input("Input value a: "))
+        b = int(input("Input value b: "))
+        result = cal_rectangle_area(a, b)
+
     else:
         result = "Wrong input"
     print(f"\nResult: {result * mult}")
